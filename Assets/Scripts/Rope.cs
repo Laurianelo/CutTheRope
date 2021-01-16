d2f0acfa1c74bff0fede108afc595a9bef01e30f
Laurianelo <32616344+Laurlo@users.noreply.github.com>
2021-01-16 21:34:57 +0100

    Weight + rope + cut

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public Rigidbody2D hook;
+    public GameObject linkPrefab;
+    public int links = 7;
+    public Weight weight;
+
+    void Start()
+    {
+        CreateRope();
+
+    }
+
+    //generate links of rope and connected them
+    private void CreateRope()
+    {
+        Rigidbody2D previousRb = hook;
+
+        for(int i = 0; i < links; i++)
+        {
+            GameObject link = Instantiate(linkPrefab, transform);
+            HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
+            joint.connectedBody = previousRb;
+
+            if(i < links - 1)
+            {
+                 previousRb = link.GetComponent<Rigidbody2D>();
+            }
+            else
+            {
+                weight.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
+            }
+          
+        }
+
+    }
+}
---
+++ Assets/Scripts/Weight.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+    public float distanceFromLastLink = 0.3f;
+
+    //Connect weight to the last link of rope
+  public void ConnectRopeEnd( Rigidbody2D endRb)
+    {
+        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedBody = endRb;
+        joint.anchor = Vector2.zero;
+        joint.connectedAnchor = new Vector2(0f, -distanceFromLastLink);
+
+    }
+}
